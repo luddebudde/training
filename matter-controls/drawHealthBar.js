2257c755979f1de9860ff70d4a6eceb2c6b78cf5
luddebudde <ludwig.lindgr@gmail.com>
2023-04-15 21:18:32 +0200

    fix heaöthbar bug that was very muchy buggy

--- matter-controls/drawHealthBar.js
+++ matter-controls/drawHealthBar.js
@@ -4,7 +4,7 @@ export const drawHealthBar = (ctx, x, y, width, height, health, maxHealth) => {
   ctx.fillStyle = 'red'
   ctx.fill()
   ctx.beginPath()
-  ctx.rect(x, y, (width * health) / maxHealth, height)
+  ctx.rect(x, y, (width * Math.max(health, 0)) / maxHealth, height)
   ctx.fillStyle = 'green'
   ctx.fill()
 }
