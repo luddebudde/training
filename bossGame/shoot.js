f1202c568af3851d3060d6714b0388d09122ac0f
luddebudde <ludwig.lindgr@gmail.com>
2023-10-17 18:41:47 +0200

    .

--- bossGame/shoot.js
+++ bossGame/shoot.js
@@ -4,6 +4,7 @@ import { bullets, mousePos, worldObjects } from "./main.js";
 export const shoot = (shooter, bullet) => {
   bullets.push(bullet);
   worldObjects.push(bullet);
+
   const direction = makeDirection(shooter, mousePos);
   bullet.vel.x = -direction.x * bullet.acc;
   bullet.vel.y = -direction.y * bullet.acc;
